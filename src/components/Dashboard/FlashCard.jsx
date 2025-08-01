c799a0e313de12845b7c60cfaae7c17f2d6547b1
tgtech123 <tgtech101@gmail.com>
2025-08-01 20:34:27 +0100

    new

--- src/components/Dashboard/FlashCard.jsx
+++ src/components/Dashboard/FlashCard.jsx
@@ -8,7 +8,10 @@ export default function FlashCard({name, icon, period, variable, trend}) {
                 <div></div>
                 <div className="flex flex-col items-center">
                     <h4 className="font-semibold">{name}</h4>
-                    <p className="text-sm text-[#737373]">{period}</p>
+                    <div className="text-sm text-[#737373]">
+                    {typeof period === "string" ? <p>{period}</p> : period}
+                    </div>
+
                 </div>
                 <span className="text-xl">{icon}</span>
             </div>
