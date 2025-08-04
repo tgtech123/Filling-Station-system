3f584f658b92708e1cdeaa36c6f6037d1e5a607d
nnamdi <uzonnamdi31@gmail.com>
2025-08-04 17:28:19 +0100

    node

--- src/components/Dashboard/DailyLiveSales.jsx
+++ src/components/Dashboard/DailyLiveSales.jsx
@@ -5,7 +5,7 @@ import { columns, data } from "../../app/dashboard/shifts/shiftData";
 export default function DailyLiveSales() {
   return (
     <DisplayCard>
-      <div className="m-0 p-0 pt-6 max-h-[400px] overflow-y-auto scrollbar-hide">
+      <div className="m-0 p-0 pt-6 max-h-[445px] overflow-y-auto scrollbar-hide">
         <h4 className="mb-2 font-semibold text-gray-500">Daily Live Sales</h4>
         <Table columns={columns} data={data} />
       </div>
--- src/components/Dashboard/MainContainer.jsx
+++ src/components/Dashboard/MainContainer.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import DisplayCard from "./DisplayCard";
+// import DisplayCard from "./DisplayCard";
 import dynamic from "next/dynamic";
 import SalesSummary from "./SalesSummary";
 import DailyLiveSales from "./DailyLiveSales";
@@ -12,16 +12,18 @@ const SalesChart = dynamic(() => import("../Dashboard/SalesOverviewChart"), {
 
 export default function MainContainer() {
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <h1 className="text-[23px] font-semibold">Welcome, Oboh ThankGod</h1>
-      <div className="mt-4 grid grid-cols-1 lg:grid-cols-2 w-full gap-3">
-        <div className="col-span-2">
+      <div className="mt-4 grid grid-cols-1 lg:grid-cols-2 w-full gap-3 ">
+        <div className="col-span-1 lg:col-span-2">
           <SalesSummary />
         </div>
-        <div>
+        <div className="col-span-1 lg:col-span-1">
           <SalesChart />
         </div>
+        <div className="col-span-1 lg:col-span-1">
         <DailyLiveSales />
+        </div>
       </div>
     </div>
   );
