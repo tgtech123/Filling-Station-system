6b5fabf04c97cab70b71d62d3660592ba1115c02
nnamdi <uzonnamdi31@gmail.com>
2025-08-08 02:36:59 +0100

    new

---
+++ src/app/dashboard/dipReading/ReadingPage.jsx
@@ -0,0 +1,19 @@
+import { readingData as data } from "./readingData"
+import TankCard from "./TankCard"
+
+export default function ReadingPage() {
+    return (
+        <div className="mt-3 bg-white p-6 rounded-[24px]">
+            {data.map((item) => (
+                <TankCard 
+                    key={item.id}
+                    tankName={item.tankName}
+                    productType={item.productType}
+                    status={item.status}
+                    lastUpdated={item.lastUpdated}
+                    systemReading={item.systemReading}
+                />
+            ))}
+        </div>
+    )
+}
---
+++ src/app/dashboard/dipReading/readingData.js
@@ -0,0 +1,26 @@
+export const readingData = [
+    {
+        id: 1,
+        tankName: "A",
+        productType: "Fuel",
+        status: "Pending",
+        lastUpdated: "25/05/2025",
+        systemReading: "2,400 Litres"
+    },
+    {
+        id: 2,
+        tankName: "B",
+        productType: "Diesel",
+        status: "Deviation",
+        lastUpdated: "25/05/2025",
+        systemReading: "2,400 Litres"
+    },
+    {
+        id: 3,
+        tankName: "C",
+        productType: "Diesel",
+        status: "Matched",
+        lastUpdated: "25/05/2025",
+        systemReading: "2,400 Litres"
+    }
+]
