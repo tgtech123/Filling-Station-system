ca3b8f666729030871125012b548cb474efff2fc
nnamdi <uzonnamdi31@gmail.com>
2025-10-07 09:01:08 +0100

    new

--- src/app/dashboard/accountant/page.jsx
+++ src/app/dashboard/accountant/page.jsx
@@ -6,6 +6,8 @@ import FlashCard from "@/components/Dashboard/FlashCard";
 import { useState, useEffect } from "react";
 import { samplePerformanceData, shiftSalesData } from "./accountantData";
 import SalesExpensesChart from "./SalesExpensesChart";
+import ProductSalesOverviewChart from "./ProductSalesOverviewChart";
+import AuditReconciledSales from "./AuditedReconciledSales";
 export default function AccountantDashboard() {
   const [userData, setUserData] = useState(null);
     const [performanceTimeFilter, setPerformanceTimeFilter] = useState('This month');
@@ -37,6 +39,7 @@ export default function AccountantDashboard() {
       <div className="mt-6 grid grid-cols-1 lg:grid-cols-2 gap-4">
         <div className="col-span-1 lg:col-span-2">
 
+        {/* Shift and Sales */}
         <DisplayCard>
           <h2 className="text-2xl font-semibold">Dashboard</h2>
           <h4>Shift and sales summary</h4>
@@ -48,10 +51,21 @@ export default function AccountantDashboard() {
           </div>
         </DisplayCard>
         </div>
-
+        
+        {/* Sales Expenses Chart */}
         <DisplayCard>
           <SalesExpensesChart data={samplePerformanceData} timeFilter={performanceTimeFilter} onTimeFilterChange={setPerformanceTimeFilter}  />
         </DisplayCard>
+        {/* Product Sales Overview */}
+        <DisplayCard>
+          <ProductSalesOverviewChart />
+        </DisplayCard>
+
+        <div className="col-span-1 lg:col-span-2">
+          <DisplayCard>
+            <AuditReconciledSales />
+          </DisplayCard>
+        </div>
       </div>
     </DashboardLayout>
   );
